03dfc9ce82d16577495bd41ddf8f8958275b6827
Braden Jackson <braden.jackson@smail.astate.edu>
2022-03-03 19:50:18 -0600

    Created register and login endpoints

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,6 +1,45 @@
 "use strict";
 const db = require("./db");
+const crypto = require("crypto")
+const argon2 = require("argon2");
 
+async function createUser(username, password){
+    let create;
+    const UserID = crypto.randomUUID();
+    const hash = await argon2.hash(password);
+
+    const sql = `INSERT INTO Users values (@userID, @username, @passwordHash)`;
+
+    const smt = db.prepare(sql);
+
+    try{
+        stmt.run({"userID": userID, "username": username, "passwordHash": hash});
+        create = true;
+    }catch(err){
+        console.error(err);
+        create = false;
+    }
+    return create;
+}
+
+function getUserByUsername(username){
+    const sql = `SELECT * FORM Users WHERE username = @username`;
+
+    const stmt = db.prepare(sql);
+    const record = stmt.get({"username": username});
+
+    return record; // may be undefined
+}
+
+function deleteUserByUsername (username){
+    const sql = `DELETE FROM Users WHERE username = @username`;
+
+    const stmt = db.prepare(sql);
+    stmt.run({"username": username});
+}
 
 module.exports = {
+    createUser,
+    getUserByUsername,
+    deleteUserByUsername
 };
--- app.js
+++ app.js
@@ -8,4 +8,49 @@ const userModel = require("./Models/userModel");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.post("/register", (req, res) => { // create users
+    if (!req.body.username || !req.body.password){
+        return res.sendStatus(400);
+    }
+
+    const {username, password} = req.body;
+
+    let created = userModel.createUser(username, password);
+
+    if (!created){
+        return res.sendStatus(409); // conflict with another username
+    }
+
+    res.sendStatus(201); // created
+});
+
+app.post("/login", async (req, res) => {
+    if (!req.body.username || !req.body.password){
+        return res.sendStatus(400);
+    }
+
+    const {username, password} = req.body;
+
+    if (!userModel.getUserByUsername(username)){
+        return res.sendStatus(400);
+    }
+    
+    const {passwordHash} = user; // specifically takes passHash from user record.
+    if (await argon2.verify(passwordHash, password)){
+        res.sendStatus(200);
+    } else{
+        res.sendStatus(400);
+    }
+});
+
+app.delete("/users/:username", (req, res) => {
+    const {username} = req.params; // get username from param given
+
+    if (!userModel.deleteUserByUsername(username)){
+        return res.sendStatus(404);
+    }
+
+    res.sendStatus(200);
+});
+
 module.exports = app;
